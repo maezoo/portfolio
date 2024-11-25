8976a64a315377a01d885c552723bb7e42890f07
maezoo <aa00456@naver.com>
2024-11-25 20:55:41 +0900

    Implementing an effect when switching between menu items using javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,56 @@
-// ======================// // go to top ============
+
+// // 메뉴 전환 ============================================
+// =======================================================
+const menuItems = document.querySelectorAll('.gnb_list');
+const sectionTabs = document.querySelectorAll('.tab_item');
+const topCoding = document.querySelector('.codingMenu');
+const leftMenu = document.querySelector('#leftMenu');
+const lnbItem = document.querySelectorAll('.lnb_item');
+const sectionCoding = document.querySelector('.coding');
+
+lnbItem.forEach(leftItems => {
+  leftItems.addEventListener('click', function () {
+    lnbItem.forEach(clickedlnb => {
+      clickedlnb.style.color = '';
+      clickedlnb.style.fontWeight = '';
+    });
+
+    leftItems.style.color = '#EF5E2E';
+    leftItems.style.fontWeight = '400';
+  });
+});
+
+menuItems.forEach(button => {
+  button.addEventListener('click', function () {
+    menuItems.forEach(item => {
+      item.style.color = '';
+    });
+
+    button.style.color = '#EF5E2E';
+
+    sectionTabs.forEach(tab => {
+      tab.style.display = 'none';
+    });
+
+    const targetTabId = button.getAttribute('data-tab');
+    const targetTab = document.querySelector(`#${targetTabId}`);
+    targetTab.style.display = 'block';
+  });
+});
+
+topCoding.addEventListener('click', () => {
+  leftMenu.classList.add('visible');
+  lnbItem.forEach(clickedlnb => {
+    clickedlnb.style.color = '';
+    clickedlnb.style.fontWeight = '';
+  });
+  lnbItem[0].style.color = '#EF5E2E';
+  lnbItem[0].style.fontWeight = '400';
+
+});
+
+// ==============================================
+// // go to top =================================
 document.addEventListener('DOMContentLoaded', function () {
   const goTopBtn = document.querySelector('.go-top');
   const threshold = window.innerHeight * 1.9;
@@ -70,96 +122,3 @@ const toggleTab = () => {
 
 clickPort.addEventListener('click', toggleTab);
 clickLogo.addEventListener('click', toggleTab);
-
-// =======================================================
-// // 메뉴 전환 ============================================
-const menuItems = document.querySelectorAll('.gnb_list');
-const sectionTabs = document.querySelectorAll('.tab_item');
-
-menuItems.forEach(button => {
-  button.addEventListener('click', function () {
-    sectionTabs.forEach(tab => {
-      tab.style.display = 'none';
-    });
-
-    const targetTabId = button.getAttribute('data-tab');
-    const targetTab = document.querySelector(`#${targetTabId}`);
-    targetTab.style.display = 'block';
-  });
-});
-// =====================================================================
-// const clickImg = document.querySelector('.clone');
-// const infoShow = document.querySelector('.info_page');
-// const imgZoom = document.querySelector('.zoom_img');
-// const closeBtn = document.querySelector('.title img');
-// const backHome = document.querySelector('.codingHome');
-
-// clickImg.addEventListener('click', () => {
-//   infoShow.classList.toggle('show'); // 토글 기능으로 등장/사라짐 조절
-//   imgZoom.classList.toggle('zoom'); // 토글 기능으로 등장/사라짐 조절
-//   clickImg.classList.toggle('remove'); // 토글 기능으로 등장/사라짐 조절
-// });
-
-// closeBtn.addEventListener('click', () => {
-//   infoShow.classList.toggle('show');
-// });
-
-
-
-// 토글 기능으로 등장/사라짐 조절
-// infoShow.classList.remove('remove'); // 토글 기능으로 등장/사라짐 조절
-
-
-// clickImg.addEventListener('click', () => {
-// infoShow.classList.t('show'); // 토글 기능으로 등장/사라짐 조절
-// });
-// clickImg.addEventListener('click', () => {
-//   clickImg.style.animation = 'none'; // 애니메이션 제 ㅇ거
-//   clickImg.style.filter = 'none'; // 애니메이션 제거
-// });
-
-
-// clickImg.addEventListener('click', () => {
-//   clickImg.classList.toggle('noAni');
-// });
-
-
-// ====================
-
-// const clickProject = document.querySelector('.menu1');
-// const projectShow = document.querySelector('.project');
-
-// clickProject.addEventListener('click', () => {
-//   projectShow.classList.toggle('show');
-// });
-
-// ====================
-
-
-
-
-// =====================================================================
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
